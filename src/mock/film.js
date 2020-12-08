f045265c57d220125d5373715f09a7f6b0a014e7
Юлия Ситникова <agrael123@yandex.ru>
2020-12-08 22:54:42 +0500

    Исправляет генерацию описания

--- src/mock/film.js
+++ src/mock/film.js
@@ -1,4 +1,4 @@
-import {getRandomElement, getRandomInteger} from "../utils";
+import {getRandomElement, getRandomInteger, getRandomElements} from "../utils";
 import {generateComment} from "../mock/comment";
 import dayjs from "dayjs";
 import duration from "dayjs/plugin/duration";
@@ -125,22 +125,10 @@ const generateGenres = () => {
 };
 
 const generateDescription = () => {
-  const sentences = [
-    `Lorem ipsum dolor sit amet, consectetur adipiscing elit.`,
-    `Cras aliquet varius magna, non porta ligula feugiat eget.`,
-    `Fusce tristique felis at fermentum pharetra.`,
-    `Aliquam id orci ut lectus varius viverra.`,
-    `Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante.`,
-    `Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum.`,
-    `Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui.`,
-    `Sed sed nisi sed augue convallis suscipit in sed felis.`,
-    `Aliquam erat volutpat. Nunc fermentum tortor ac porta dapibus.`,
-    `In rutrum ac purus sit amet tempus.`
-  ];
-
+  const sentences = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet varius magna, non porta ligula feugiat eget. Fusce tristique felis at fermentum pharetra. Aliquam id orci ut lectus varius viverra. Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante. Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum. Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui. Sed sed nisi sed augue convallis suscipit in sed felis. Aliquam erat volutpat. Nunc fermentum tortor ac porta dapibus. In rutrum ac purus sit amet tempus.`.split(`. `);
   const sentencesCount = getRandomInteger(1, 5);
 
-  return new Array(sentencesCount).fill().map(() => getRandomElement(sentences)).join(` `);
+  return `${getRandomElements(sentences, sentencesCount).join(`. `)}.`;
 };
 
 const generateAgeLimit = () => {
--- src/utils.js
+++ src/utils.js
@@ -7,3 +7,15 @@ export const getRandomInteger = (a = 1, b = 0) => {
 export const getRandomElement = (elements) => {
   return elements[Math.floor(Math.random() * (elements.length))];
 };
+
+export const getRandomElements = (elements, count) => {
+  const randomElements = elements.slice();
+  for (let i = randomElements.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * randomElements.length);
+    const swap = randomElements[j];
+    randomElements[j] = randomElements[i];
+    randomElements[i] = swap;
+  }
+  return randomElements.slice(0, count);
+};
+
